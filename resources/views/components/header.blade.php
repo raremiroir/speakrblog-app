09cdc15e542e2180c969b8d1aa0505b5544db272
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-19 03:18:22 +0100

    Add header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<div class="flex w-4/5 mx-auto justify-between items-center">
+   <h1 class="uppercase text-4xl font-title text-gray-800 dark:text-gray-200 tracking-wider border-b-gray-800 dark:border-b-gray-200 border-b-2 w-fit">
+       {{ $slot }}
+   </h1>
+    {{ $append?? '' }}
+</div>
