ad6f34056ed0da0767de747397955cb8840fa53d
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-19 03:18:28 +0100

    Add button component

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,10 @@
+<button
+   class="
+      transition-all duration-300 ease-in-out
+      bg-gray-400 dark:bg-gray-600 
+      py-2 px-4 font-medium 
+      text-gray-800 dark:text-gray-100
+      rounded-lg
+      hover:bg-success-d1 dark:hover:bg-success/80">
+      {{ $slot }}
+</button>
