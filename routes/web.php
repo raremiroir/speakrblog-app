63d2c310ecc9cae2b73f1d838522a10140629af5
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-19 03:19:06 +0100

    Edit routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
@@ -16,25 +17,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('/welcome', function () {
-    return view('welcome');
-});
 
-Route::get('/posts', function () {
-    return view('home');
-})->middleware(['auth', 'verified'])->name('posts');
+// Public routes
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
-Route::middleware('auth')->group(function () {
+Route::get('/home', [PostController::class, 'index'])->name('home');
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/posts/add', [PostController::class, 'create'])->name('posts.add');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/tags/{tag}', [PostController::class, 'indexByTag'])->name('posts.indexByTag');
+
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-// Route::resource('posts', PostController::class)
-//     ->only(['index', 'store'])
-//     ->middleware(['auth', 'verified']);
+    // Posts routes
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+    // Comments routes
+    // Route::get('/posts/{post}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+    // Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    // Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+    // Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    // Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+    // Friendships routes
+    // Route::get('/users/search', [FriendshipController::class, 'search'])->name('users.search');
+    // Route::get('/users/{user}/friends', [FriendshipController::class, 'index'])->name('users.friends');
+    // Route::post('/users/{user}/friendship', [FriendshipController::class, 'store'])->name('users.friendship.store');
+    // Route::delete('/users/{user}/friendship', [FriendshipController::class, 'destroy'])->name('users.friendship.destroy');
+});
 
 require __DIR__.'/auth.php';
