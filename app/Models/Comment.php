70bbac03ebf3e51ba2bab9d79455f293ea7bdd1e
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-19 03:17:48 +0100

    Add Comment Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+        'post_id',
+        // 'parent_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'user_likes_comment')->withTimestamps();
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
