702d8307346c357008ea4aad206bd5fd40f20044
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 15:56:12 +0100

    Username unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username');
+        Schema::table('users', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
