afc9dc2aae66de4126a5cf3f53f7f9c394af94dc
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 15:55:19 +0100

    Integrade posts grid component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,9 +12,5 @@
         </x-header>
     </x-slot>
 
-    <div class="my-16 flex flex-col gap-8 items-center justify-start">
-        @foreach ($posts as $post)
-            <x-post-card :post="$post" />
-        @endforeach
-    </div>
+    <x-posts-grid :posts="$posts"/>
 </x-app-layout>
