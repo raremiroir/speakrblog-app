10dae9c9646131e5cb91cf1eafa38492a9196dbd
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 00:20:20 +0100

    Fix text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,19 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input 
+   {{ $disabled ? 'disabled' : '' }} 
+   {{ $attributes }}
+   class="
+      transition-all duration-200 ease-in-out
+      w-full
+      border-transparent 
+      bg-gray-300/40
+      tracking-wider 
+      font-semibold dark:font-medium
+      dark:bg-gray-800
+      placeholder:italic
+      focus:text-black dark:focus:text-white
+      text-success-d3 dark:text-success-l3 
+      focus:border-success dark:focus:border-success-l1 focus:ring-success dark:focus:ring-success-l1 
+      rounded-md shadow-sm"
+/>
