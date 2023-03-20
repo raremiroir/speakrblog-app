c23325bcf2280bd98c00cba6423f3e7b6a380a81
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 23:17:04 +0100

    Add fulllogo component

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,9 @@
+<div class="shrink-0 flex items-center">
+   <a href="{{ route('posts.index') }}" class="flex gap-2 items-center justify-start">
+       <x-application-logo class="block h-10 w-auto fill-current" />
+       <div class="flex flex-col gap-0 items-start justify-center">
+           <span class="text-success dark:text-success-l2 font-title text-xl">SPEAKR</span>
+           <span class="text-gray-600 dark:text-success-l2/80 text-xs">Speak your Mind</span>
+       </div>
+   </a>
+</div>
