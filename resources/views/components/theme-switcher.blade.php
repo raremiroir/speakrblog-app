18ef44c5ceb8815ed71284a8a0cf26ce88fa96f8
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 17:48:54 +0100

    Integrate btn component

--- resources/views/components/theme-switcher.blade.php
+++ resources/views/components/theme-switcher.blade.php
@@ -1,25 +1,18 @@
-<button 
+<x-button 
    title="Switch theme"
    id="switch-theme-btn" 
-   class="
-    bg-gray-200 dark:bg-gray-700 
-    hover:bg-gray-300 dark:hover:bg-gray-600
-    text-gray-800 dark:text-gray-300
-      hover:text-gray-900 dark:hover:text-gray-200
-      hover:shadow-md dark:hover:shadow-none
-      active:translate-y-1 active:shadow-none
-      h-10 w-10 rounded-xl
-      transtion-all duration-200 ease-out
-      "
-   aria-label="Switch Theme">
+   color="default"
+   size="sm" square>
    <div id="switch-btn-inner" class="-rotate-12">
       @if (session('theme') == 'dark')
-         <i class="fas fa-sun"></i>
-      @else
          <i class="fas fa-moon"></i>
+      @else
+         <i class="fas fa-sun"></i>
       @endif
    </div>
-</button>
+</x-button>
+
+
 
 
 <script>
@@ -27,11 +20,14 @@
    const innerDiv = document.querySelector('#switch-btn-inner');
 
    switchThemeBtn.addEventListener('click', () => {
-      document.querySelector('html').classList.toggle('dark');
       if (document.querySelector('html').classList.contains('dark')) {
-         innerDiv.innerHTML = '<i class="fas fa-sun"></i>';
-      } else {
+         document.querySelector('html').classList.remove('dark');
+         sessionStorage.setItem('theme', 'dark');
          innerDiv.innerHTML = '<i class="fas fa-moon"></i>';
+      } else {
+         document.querySelector('html').classList.add('dark');
+         sessionStorage.setItem('theme', 'light');
+         innerDiv.innerHTML = '<i class="fas fa-sun"></i>';
       }
    });
 </script>
