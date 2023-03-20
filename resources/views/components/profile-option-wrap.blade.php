93fcb1ed5619e3ca9f575e446c784a21984592fa
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 23:19:23 +0100

    Edit width

--- resources/views/components/profile-option-wrap.blade.php
+++ resources/views/components/profile-option-wrap.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 sm:p-8 w-full bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+<div class="p-4 sm:p-8 w-full lg:w-4/5 xl:w-full xl:max-w-2xl bg-white dark:bg-gray-800 shadow sm:rounded-lg">
     <div class="max-w-xl">
         {{ $slot }}
     </div>
