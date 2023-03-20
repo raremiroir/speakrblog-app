31a67f47429444cfcecf34884524c1ff3dd113ad
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 14:25:35 +0100

    Add functions to commentcontroller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,63 +3,69 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Display all comments
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    // Display form to create a new comment
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    // Store a new comment in the database
+    public function store(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'body' => 'required',
+        ]);
+
+        $comment = new Comment();
+        $comment->body = $request->input('body');
+        $comment->user_id = Auth::id();
+        $post->comments()->save($comment);
+
+        return redirect()->route('posts.show', $post);
     }
 
-    /**
-     * Display the specified resource.
-     */
+    // Display a single comment
     public function show(Comment $comment)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
+    // Display form to edit existing comment
+    public function edit(Post $post, Comment $comment)
     {
-        //
+        return view('comments.edit', compact('post', 'comment'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Comment $comment)
+    // Update existing comment in the database
+    public function update(Request $request, Post $post, Comment $comment)
     {
-        //
+        $request->validate([
+            'body' => 'required',
+        ]);
+
+        $comment->body = $request->input('body');
+        $comment->save();
+
+        return redirect()->route('posts.show', $post);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Comment $comment)
+    // Delete existing comment from the database
+    public function destroy(Post $post, Comment $comment)
     {
-        //
+        $comment->delete();
+
+        return redirect()->route('posts.show', $post);
     }
 }
