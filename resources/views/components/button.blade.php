9af945ef58bf8ddc10753479ea4a5197f51c06fe
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 14:25:51 +0100

    Recreate button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,10 +1,51 @@
-<button
-   class="
-      transition-all duration-300 ease-in-out
-      bg-gray-400 dark:bg-gray-600 
-      py-2 px-4 font-medium 
-      text-gray-800 dark:text-gray-100
-      rounded-lg
-      hover:bg-success-d1 dark:hover:bg-success/80">
+@props([
+   'size' => 'md',
+   'color' => 'default',
+   'outlined' => false,
+   'block' => false,
+   'square' => false,
+   'circle' => false,
+   'flat' => false,
+   'uppercase' => false,
+])
+
+@php
+   $colors = [
+      'default'   => 'text-gray-800 dark:text-gray-200 border-gray-200 dark:border-gray-700 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 hover:border-gray-300 dark:hover:bg-gray-600 dark:hover:border-gray-600 focus:bg-gray-200 dark:focus:bg-gray-700 active:bg-gray-400 dark:active:bg-gray-500 focus:border-gray-400 dark:focus:border-gray-500',
+      'primary'   => 'text-gray-800 dark:text-gray-200 border-gray-200 dark:border-gray-700 bg-gray-200 dark:bg-gray-700 hover:bg-success hover:border-success hover:text-gray-200 dark:hover:border-success-l1 dark:hover:bg-success-l1 dark:hover:text-gray-800 focus:bg-gray-200 dark:focus:bg-gray-700 active:bg-success-d1 active:text-gray-100 dark:active:bg-success-l3 dark:active:text-gray-800 dark:active:border-success-l3 focus:text-gray-800 dark:focus:text-gray-200 focus:border-success dark:focus:border-success-l2',
+      'transp'    => '!shadow-none bg-transparent text-gray-800 dark:text-gray-200 hover:bg-gray-400 dark:hover:bg-gray-600 hover:bg-opacity-25 focus:bg-gray-400 dark:focus:bg-gray-600 focus:bg-opacity-25 active:bg-gray-500 active:bg-opacity-25 focus:border-gray-400 dark:focus:border-gray-500',
+      'success'   => 'border-success bg-success text-gray-200 hover:border-success-d1 hover:bg-success-d1 focus:bg-success-d1 focus:border-success-l1/60 active:border-success-d2 active:bg-success-d2',
+      'error'     => 'border-error bg-error text-gray-200 hover:border-error-d1 hover:bg-error-d1 focus:bg-error-d1 focus:border-error-l1/60 active:border-error-d2 active:bg-error-d2',
+      'warning'   => 'border-warning bg-warning text-gray-800 hover:border-warning-d1 hover:bg-warning-d1 focus:bg-warning-d1 focus:border-warning-l1/60 active:border-warning-d2 active:bg-warning-d2',
+      'info'      => 'border-info bg-info text-gray-200 hover:border-info-d1 hover:bg-info-d1 focus:bg-info-d1 focus:border-info-l1/60 active:border-info-d2 active:bg-info-d2',
+   ];
+   $sizes = [
+      'xs'  => 'px-2 py-1 text-xs   leading-none tracking-wide',
+      'sm'  => 'px-3 py-2 text-sm   leading-4 tracking-wide',
+      'md'  => 'px-4 py-2 text-sm   leading-5 tracking-wide',
+      'lg'  => 'px-4 py-2 text-base leading-6 tracking-wider',
+      'xl'  => 'px-6 py-3 text-base leading-6 tracking-wider',
+      'xxl' => 'px-8 py-4 text-lg   leading-6 tracking-wider'
+   ];
+
+   $colorClass = $colors[$color];
+   $sizeClass = $sizes[$size];
+   $outlinedClass = $outlined ? '!bg-transparent !shadow-none !text-gray-800 dark:!text-gray-200' : 'border-transparent';
+   $blockClass = $block ? 'w-full' : '';
+   $squareClass = $square ? 'aspect-square' : '';
+   $circleClass = $circle ? 'aspect-square rounded-full' : 'rounded-md';
+   $flatClass = $flat ? 'shadow-none' : 'shadow-md';
+   $uppercaseClass = $uppercase ? 'uppercase' : '';
+
+   $active = 'active:translate-y-0.5';
+   $transition = 'transition-all duration-300 ease-in-out';
+
+   $default = 'font-semibold border-2';
+
+   $classes = "$transition $default $active $outlinedClass $colorClass $sizeClass $blockClass $squareClass $circleClass $flatClass $uppercaseClass";
+
+@endphp
+
+<button class="{{$classes}}">
       {{ $slot }}
 </button>
