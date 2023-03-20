6225c0a5d57b03a868469c3617a08ea4744b2e34
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-20 23:17:38 +0100

    Add logo

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,7 @@
             {{ __('All Speaks') }}
             <x-slot name="append">
                 <x-button color="primary" href="{{ route('posts.add') }}">
+                    <x-application-logo class="block h-6 w-auto dark:fill-white dark:group-hover:fill-black fill-gray-800 group-hover:fill-gray-200 transition-all duration-200 ease-out" />
                     Speak Something!
                 </x-button>
             </x-slot>
