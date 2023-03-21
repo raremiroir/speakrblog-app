4043c946f445fa7a8ab1163071358c206fb215c1
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 02:50:10 +0100

    Add size prop

--- resources/views/components/profile-avatar.blade.php
+++ resources/views/components/profile-avatar.blade.php
@@ -1,7 +1,13 @@
 @props([
    'user' => null,
+   'size' => 24,
 ])
 @php
    $user = $user ?? auth()->user();
+   $size = $size ?? 24;
 @endphp
-<img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="m-0 rounded-full border-2 border-success dark:border-success-l1 p-[1px]" width="24" height="24">
+<img 
+   src="{{ $user->avatar }}" alt="{{ $user->name }}" 
+   class="m-0 rounded-full border-2 border-success dark:border-success-l1 p-[1px]" 
+   width="{{ $size }}" height="{{ $size }}"
+   >
