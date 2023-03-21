0769944ccf2defee3a7d5f84968644ef3fea6fbd
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 21:21:55 +0100

    Extend app logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,10 +16,12 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+            <div class="flex items-center gap-4">
+                <x-application-logo class="block h-20 w-auto fill-current" />
+                <div class="flex flex-col gap-0 items-start justify-center">
+                    <span class="text-success dark:text-success-l2 font-title text-3xl">SPEAKR</span>
+                    <span class="text-gray-600 dark:text-success-l2/80 text-md">Speak your Mind</span>
+                </div>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
