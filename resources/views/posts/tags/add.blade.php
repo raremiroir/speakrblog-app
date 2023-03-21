0a5cb7390f055037aa3ba311a49729e8238aeca6
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 21:21:35 +0100

    Use wireui color picker

--- resources/views/posts/tags/add.blade.php
+++ resources/views/posts/tags/add.blade.php
@@ -1,19 +1,39 @@
 <form 
    action="{{ route('posts.tags.store') }}" method="POST" 
-   class="bg-gray-700/10 my-4 py-2 px-4 flex flex-col gap-4 ">
+   class="bg-gray-700/10 my-4 p-2 w-fit mx-auto rounded-xl flex flex-col gap-4 ">
    @csrf
-   <div class="flex flex-row gap-4">
-      <div class="flex flex-col gap-2 items-start w-full">
-         <label class="font-title text-xl text-gray-600 dark:text-gray-400" for="name">Add a Tag</label>
-         <x-text-input name="name" id="name" aria-autocomplete="none" required/>
+   <div class="flex flex-row gap-4 w-fit">
+      <div class="flex flex-col gap-2 items-start w-fit">
+         <x-input 
+            label="Add a Tag" 
+            name="name" id="name" 
+            aria-autocomplete="none" 
+            required placeholder="New Tag"
+            class="w-80"
+         />
       </div>
       <div class="flex flex-col gap-2 items-start w-fit relative">
          {{-- <x-color-input /> --}}
-         <x-color-picker 
-            name="color" id="color" 
-            :options="['opacity' => true, 'format' => 'hex']"
-         />
+         <div class="relative h-full whitespace-nowrap">
+            <x-color-picker
+               label="Pick a Color"
+               placeholder="Pick a Color"
+               color-name-as-value required
+               name="color" id="color" 
+               :colors="[
+                  [ 'name' => 'Green',      'value' => '#318748' ],
+                  [ 'name' => 'Blue',       'value' => '#336699' ],
+                  [ 'name' => 'Red',        'value' => '#BF0603' ],
+                  [ 'name' => 'Yellow',     'value' => '#FFA900' ],
+
+                  [ 'name' => 'Aquamarine', 'value' => '#6BFFB8' ],
+                  [ 'name' => 'Orange',     'value' => '#D16014' ],
+                  [ 'name' => 'Pink',       'value' => '#E086D3' ],
+                  [ 'name' => 'Cyan',       'value' => '#80FFEC' ],
+               ]"
+            />
+         </div>
       </div>
    </div>
-   <x-button type="submit" size="sm" id="add-tag-btn" class="whitespace-nowrap" color="primary">Add new tag</x-button>
+   <x-btn type="submit" size="sm" id="add-tag-btn" class="whitespace-nowrap" color="primary">Add new tag</x-btn>
 </form>
