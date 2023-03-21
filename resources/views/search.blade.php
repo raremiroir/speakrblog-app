2351c427adb2009e8a0ab0bc4aec16f63d2bfd09
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 12:40:36 +0100

    Add search view

---
+++ resources/views/search.blade.php
@@ -0,0 +1,27 @@
+<p class="text-xl font-medium text-gray-600 dark:text-gray-400">
+   Search Results for "<span class="font-semibold text-success-d2 dark:text-success-l2">{{ $search }}</span>"
+</p>
+
+<div class="py-12 w-full">
+   <span class="text-lg text-gray-700 dark:text-gray-300 font-medium">Posts</span>
+   <ul class="w-full">
+      @if ($posts->count() > 0)
+         @foreach ($posts as $post)
+            <x-post-card :post="$post" />
+         @endforeach
+      @else
+         <li class="text-gray-400 dark:text-gray-500">No posts found</li>
+      @endif
+   </ul>
+
+   <span class="text-lg text-gray-700 dark:text-gray-300 font-medium">Users</span>
+   <ul>
+      @if ($users->count() > 0)
+         @foreach ($users as $user)
+            <x-user-card :user="$user" />
+         @endforeach
+      @else
+         <li class="text-gray-400 dark:text-gray-500">No users found</li>
+      @endif
+   </ul>
+</div>
