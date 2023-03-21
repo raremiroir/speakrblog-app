ab75dfb86d753d745584907087d45393aeeb91a9
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 17:04:40 +0100

    Add functions for tags

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,9 +19,10 @@ class PostController extends Controller
 
     // Display form to create a new post
     public function create(Request $request): View {
-        return view('posts.add', [
-            'user' => $request->user(),
-        ]);
+        $tags = Tag::all();
+        $user = $request->user();
+
+        return view('posts.add', compact('tags', 'user'));
     }
 
     // Store a new post in the database
@@ -33,9 +35,10 @@ class PostController extends Controller
         $post = new Post();
         $post->title = $request->input('title');
         $post->body = $request->input('body');
-        // $post->user_id = auth()->user()->id;
         $post->user_id = Auth::id();
         $post->save();
+
+        $post->tags()->sync($request->input('tags') ?: []);
 
         return redirect()->route('posts.show', $post);
     }
@@ -49,7 +52,10 @@ class PostController extends Controller
 
     // Display form to edit existing post
     public function edit(Post $post) {
-        return view('posts.edit', compact('post'));
+
+        $tags = Tag::all();
+
+        return view('posts.edit', compact('post', 'tags'));
     }
 
     // Update existing post in the database
@@ -62,6 +68,8 @@ class PostController extends Controller
         $post->title = $request->input('title');
         $post->body = $request->input('body');
         $post->save();
+
+        $post->tags()->sync($request->input('tags') ?: []);
 
         return redirect()->route('posts.show', $post);
     }
@@ -90,4 +98,36 @@ class PostController extends Controller
         $users = User::search($search)->get();
         return view('search', compact('posts', 'users', 'search'));
     }
+
+    // Display posts by a specific user
+    public function userPosts(User $user) {
+        $posts = $user->posts()->with('user')->latest()->paginate(20);
+        return view('posts.index', compact('posts'));
+    }
+
+    // Display posts by a specific tag
+    public function tagPosts(Tag $tag) {
+        $posts = $tag->posts()->with('user')->latest()->paginate(20);
+        return view('posts.index', compact('posts'));
+    }
+
+    // Display posts by a specific user and tag
+    public function userTagPosts(User $user, Tag $tag) {
+        $posts = $user->posts()->whereHas('tags', function ($query) use ($tag) {
+            $query->where('tags.id', $tag->id);
+        })->with('user')->latest()->paginate(20);
+        return view('posts.index', compact('posts'));
+    }
+
+    // Display all tags for a specific post
+    public function tags(Post $post) {
+
+        $tags = [];
+        foreach ($post->tags as $tag) {
+            $tags[] = $tag->pivot->name;
+        }
+        return view('posts.tags.index', compact('tags'));
+    }
+
+    
 }
