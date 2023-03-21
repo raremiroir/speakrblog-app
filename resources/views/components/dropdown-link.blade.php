87a1538ab7f56e2dc4e79a66768059ddbf3b4006
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-21 11:31:44 +0100

    Change text color

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,5 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-success dark:hover:bg-success-d2 focus:outline-none focus:bg-gray-100 dark:focus:bg-success-d3 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a 
+   {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:text-gray-200 dark:text-gray-300 hover:bg-success dark:hover:bg-success-d2 focus:outline-none focus:bg-gray-100 dark:focus:bg-success-d3 transition duration-150 ease-in-out']) }}
+   >
+   {{ $slot }}
+</a>
