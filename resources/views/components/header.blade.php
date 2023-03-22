76c0ffab129197932413aca4071e1f481ee263b4
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-22 14:59:31 +0100

    Add whitespace-nowrap

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="flex w-4/5 mx-auto justify-between items-center">
-   <h1 class="uppercase text-4xl font-title text-gray-800 dark:text-gray-200 tracking-wider border-b-gray-800 dark:border-b-gray-200 border-b-2 w-fit">
+   <h1 class="uppercase text-4xl font-title text-gray-800 dark:text-gray-200 tracking-wider border-b-gray-800 dark:border-b-gray-200 border-b-2 w-fit whitespace-nowrap">
        {{ $slot }}
    </h1>
     {{ $append?? '' }}
