4bd50755728edf429c2303a8d6cf12415a149eaa
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-22 19:56:17 +0100

    Edit lang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 @endphp
 
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
