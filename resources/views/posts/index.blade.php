296be773a2e7bbaa05a948e41eccb9ae65e54636
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-22 13:25:03 +0100

    Remove localization string

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <x-header>
-            {{ __('All Speaks') }}
+            All Speaks
             <x-slot name="append">
                 <x-btn color="primary" href="{{ route('posts.add') }}">
                     <x-application-logo class="block h-6 w-auto dark:fill-white dark:group-hover:fill-black fill-gray-800 group-hover:fill-gray-200 transition-all duration-200 ease-out" />
