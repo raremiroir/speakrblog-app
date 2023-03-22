23f8dc97187aedadfb9bcaa16666a6f7f96ba053
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-22 01:59:17 +0100

    Integrate multiselect

--- resources/views/posts/add.blade.php
+++ resources/views/posts/add.blade.php
@@ -1,95 +1,83 @@
 @php
+    // Random placeholder text for the textarea
     $placeholders = [
-        'The speaker\'s words were like a river, flowing endlessly and never to wither.',
-        'The speaker held their audience in thrall, weaving a tale that enthralled them all.',
-        'The floor is yours.',
-        'Start speaking your mind!',
-        'Speak your truth.',
-        'Unleash your inner chatterbox.',
-        'Speaking of speaking, how about sharing your latest brainwave with us?',
-        'The keyboard is your stage, the text area your mic.',
-        'Warning: speaking your mind may cause increased clarity and a sense of relief.',
-        'What\'s the point of having a mouth if you don\'t use it? Type away and speak your truth!',
-        'We can\'t read your mind. Start speaking already!',
-        'Speak into existence!',
-        'Silence may be golden, but your words are pure platinum.',
-        'There\'s a time to listen and a time to speak. This is definitely the latter.',
-        'Put your keyboard where your mouth is!',
-        'What do you call a talkative keyboard?',
-        'Speak of the devil and he shall appear.',
+        'The speaker\'s words were like a river, flowing endlessly and never to wither.', 
+        'The speaker held their audience in thrall, 
+        weaving a tale that enthralled them all.', 
+        'The floor is yours.', 
+        'Start speaking your mind!', 
+        'Speak your truth.', 
+        'Unleash your inner chatterbox.', 
+        'Speaking of speaking, 
+        how about sharing your latest brainwave with us?', 
+        'The keyboard is your stage, the text area your mic.', 
+        'Warning: speaking your mind may cause increased clarity and a sense of relief.', 
+        'What\'s the point of having a mouth if you don\'t use it? Type away and speak your truth!', 
+        'We can\'t read your mind. Start speaking already!', 
+        'Speak into existence!', 
+        'Silence may be golden, 
+        but your words are pure platinum.', 
+        'There\'s a time to listen and a time to speak. This is definitely the latter.', 
+        'Put your keyboard where your mouth is!', 
+        'What do you call a talkative keyboard?', 
+        'Speak of the devil and he shall appear.'
     ];
-
+    // Get a random placeholder
     $random_key = array_rand($placeholders);
 @endphp
 
+{{-- View --}}
 <x-app-layout>
-   <x-slot name="header">
+    {{-- Header --}}
+    <x-slot name="header">
         <x-header>
-            {{ __('Speak Anything!') }}
+            Speak Anything!
         </x-header>
-   </x-slot>
+    </x-slot>
 
-   <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
+    {{-- Content --}}
+    <div class="max-w-5xl mx-auto p-4 sm:p-6 lg:p-8">
         @if (!Auth::check())
             <div class="bg-gray-100 dark:bg-gray-800 p-4 rounded-md">
-                {{ __('You are not allowed to create posts. Please log in or create an account.') }}
+                You are not allowed to create posts. Please log in or create an account.
             </div>
         @else
-            <form method="post">
-                <div class="">
-                    <label for="new-tag">Add new tag:</label>
-                    <input type="text" name="new-tag" id="new-tag" class="form-control">
-                    <x-btn size="sm" id="add-tag-btn">Add new tag</x-btn>
-                </div>    
-            </form>
-            <form method="POST" action="{{ route('posts.store') }}">
+            {{-- Add Tag Form --}}
+
+            {{-- Add Post Form --}}
+            <form method="POST" action="{{ route('posts.store') }}" class="z-0 isolate">
                 @csrf
 
-                {{-- Tags --}}
+                {{-- Select Tag --}}
                 <div class="">
-                    <div class="">
-                        <x-input-label for="tags" :value="__('Tags:')" />
-                        <select name="tags[]" id="tags" class="w-full" multiple>
-                            @foreach ($tags as $tag)
-                                <option value="{{ $tag->id }}" {{ in_array($tag->id, old('tags', [])) ? 'selected' : '' }}>
-                                    {{ $tag->name }}
-                                </option>
-                            @endforeach
-                            <small class="form-text text-muted">Select existing tags or add a new tag by typing it in and pressing "Enter".</small>
-                        </select>
-    
-                        {{-- <x-select id="category" name="category" required>
-                            <option value="">{{ __('Select a category') }}</option>
-                            @foreach ($categories as $category)
-                                <option value="{{ $category->id }}" {{ old('category') == $category->id ? 'selected' : '' }}>
-                                    {{ $category->name }}
-                                </option>
-                            @endforeach
-                        </x-select> --}}
-                    </div>
-                    
+                    @include('components.multi-select', 
+                    [
+                        'name' => 'my-multi-select',
+                        'id' => 'my-multi-select',
+                        'label' => 'My Multi-Select',
+                        'placeholder' => 'Select options...',
+                        'options' => App\Models\Tag::getTagsAsArray(),
+                        'required' => true,
+                        'disabled' => false,
+                    ])
+                </div>
+
+                {{-- Title --}}
+                <div class="mb-4">
+                    <x-input label="Title" type="text" name="title" id="title" placeholder='An appealing title'
+                        required autofocus />
                 </div>
 
                 <div class="mb-4">
-                    <x-input-label for="title" :value="__('Title')" />
-                    <x-text-input 
-                        id="title" name="title" 
-                        type="text" :value="old('title')" 
-                        placeholder="Title"
-                        required autofocus 
-                    />
-                </div>
-        
-                <div class="mb-4">
-                    <x-input-label for="body" :value="__('Body')" />
-                    <x-textarea id="body" name="body" rows="10" required placeholder="{{ $placeholders[$random_key] }}">
+                    <x-textarea label="Post Content" type="text" name="body" id="body"
+                        placeholder="{{ $placeholders[$random_key] }}" required autofocus rows="10">
                         {{ old('body') }}
                     </x-textarea>
                 </div>
-        
+
                 <div class="flex items-center justify-end mt-4">
                     <x-btn type="submit" color="success" size="lg">
-                        {{ __('Speak!') }}
+                        Speak!
                     </x-btn>
                 </div>
             </form>
