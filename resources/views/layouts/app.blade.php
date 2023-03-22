96fbc94415f9fc9d9adf0ebb116675a26a304e16
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-22 15:02:37 +0100

    Add comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,11 @@
         <link href="https://fonts.googleapis.com/css2?family=Marmelad&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @wireUiScripts
-        <script src="//unpkg.com/alpinejs" defer></script>
+            {{-- Vite --}}
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+            {{-- Wire UI --}}
+            @wireUiScripts
+            <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -86,5 +88,6 @@
                 }
             });
         </script>
+        @stack('scripts')
     </body>
 </html>
