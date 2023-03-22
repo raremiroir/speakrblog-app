dbfb1bf1ab5b8768add65309be0371fc9c82e221
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-23 00:06:40 +0100

    Add tag selection

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,27 +1,62 @@
-<x-app-layout>
-   <x-slot name="header">
+<x-app-layout hasAside asideTitle="Add a Tag">
+    {{-- Header --}}
+    <x-slot name="header">
         <x-header>
-            {{ __('Edit your Speak') }}
+            Edit your Speak
         </x-header>
-   </x-slot>
+    </x-slot>
 
-   <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    {{-- Aside - Add tag --}}
+    <x-slot name="aside">
+        @include('posts.tags.add')
+        <x-section title="All Tags">
+            <div class="flex flex-row flex-wrap gap-2">
+                @foreach($tags as $tag)
+                    @include('posts.tags.tag', [
+                        'tag' => $tag, 
+                        'size' => 'md'
+                        ])
+                @endforeach
+            </div>
+        </x-section>
+    </x-slot>
+
+    {{-- Content --}}
+    <x-section title="Edit {{ $post->title }}">
         @if (!Auth::check() || !Auth::user()->username === $post->user->username)
             <div class="bg-gray-100 dark:bg-gray-800 p-4 rounded-md">
-                {{ __('You are not allowed to create posts. Please log in or create an account.') }}
+                You are not allowed to create posts. Please log in or create an account.
             </div>
         @else
+            {{-- Edit Post Form --}}
             <form method="POST" action="{{ route('posts.update', $post->id) }}">
                 @csrf
                 @method('PUT')
+
+                {{-- Select Tag --}}
+                <div class="">
+                    @include('components.multi-select', 
+                    [
+                        'name' => 'tags',
+                        'id' => 'tags',
+                        'label' => 'Select Tags',
+                        'placeholder' => 'Select Tags...',
+                        'options' => App\Models\Tag::getTagsAsArray(),
+                        'required' => false,
+                        'disabled' => false,
+                        'selectedOptions' => $post->tags->pluck('name')->toArray(),
+                    ])
+                </div>
+                <hr class="border-gray-300 dark:border-gray-700 mx-auto mt-16 mb-8"/>
         
+                {{-- Title --}}
                 <div class="mb-4">
-                    <x-input-label for="title" :value="__('Title')" />
+                    <x-input-label for="title" :value="'Title'" />
                     <input id="title" class="form-input rounded-md shadow-sm mt-1 block w-full"  type="text"  name="title" value="{{old('title', $post->title)}}" required autofocus />
                 </div>
         
                 <div class="mb-4">
-                    <x-input-label for="body" :value="__('Body')" />
+                    <x-input-label for="body" :value="'Post Content'" />
                     <textarea id="body" name="body" class="form-input rounded-md shadow-sm mt-1 block w-full" rows="10" required>{{ old('body', $post->body) }}</textarea>
                 </div>
 
@@ -30,14 +65,14 @@
                 <div class="flex items-center justify-end mt-4 gap-4">
                     <a href={{ route('posts.show', $post->id) }}>
                         <x-btn color="error" size="lg" class="opacity-80">
-                            {{ __('Cancel') }}
+                            Cancel
                         </x-btn>
                     </a>
                     <x-btn type="submit" color="success" size="lg">
-                        {{ __('Save Speak') }}
+                        Save Speak
                     </x-btn>
                 </div>
             </form>
         @endif
-    </div>
+    </x-section>
 </x-app-layout>
