10493dd09d8fc7353468b9cf73aedbb8a0547b09
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-23 01:09:48 +0100

    Edit color

--- resources/views/profile/info/amt-comments.blade.php
+++ resources/views/profile/info/amt-comments.blade.php
@@ -1,4 +1,4 @@
-<x-button flat warning md title="{{ $user->comments->count() }} {{ Str::plural('comment', $user->comments->count()) }}">
+<x-button flat orange md title="{{ $user->comments->count() }} {{ Str::plural('comment', $user->comments->count()) }}">
    <div class="flex flex-row items-center gap-1 font-semibold">
       <i class="fas fa-comment-alt"></i>
       {{ $user->comments->count() }} @if (isset($extend) && $extend){{ Str::plural('comment', $user->comments->count()) }}@endif
