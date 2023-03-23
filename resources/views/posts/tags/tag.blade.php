9d6913ce43281dcd19e5473c6d30a456eacedacd
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-23 01:09:16 +0100

    Move function to top

--- resources/views/posts/tags/tag.blade.php
+++ resources/views/posts/tags/tag.blade.php
@@ -6,29 +6,6 @@
 ])
 
 @php
-   $sizes = [
-      'sm' => 'text-xs px-1 py-0.5',
-      'md' => 'text-sm px-2 py-1',
-      'lg' => 'text-md px-3 py-[6px]',
-   ];
-   $size = $sizes[$size];
-   $class = $class ?? '';
-   $class = $size . ' ' . $class;
-
-   $color = $tag->color ?? 'Gray';
-   $hsl = hex2hsl($color);
-   $textColor = 'text-black';
-   
-   /* Convert hex color to hsl */
-   // Get lightness value from hsl
-   $lightness = $hsl[2];
-   // Set text color based on lightness value
-   if ($lightness > 0.4) {
-      $textColor = 'text-gray-900 font-bold';
-   } else {
-      $textColor = 'text-gray-50 font-semibold';
-   }
-
    if (! function_exists('hex2hsl')) {
       function hex2hsl($hex) {
          $hex = str_replace('#', '', $hex);
@@ -59,6 +36,29 @@
          }
          return array($h, $s, $l);
       }
+   }
+
+   $sizes = [
+      'sm' => 'text-xs px-1 py-0.5',
+      'md' => 'text-sm px-2 py-1',
+      'lg' => 'text-md px-3 py-[6px]',
+   ];
+   $size = $sizes[$size];
+   $class = $class ?? '';
+   $class = $size . ' ' . $class;
+
+   $color = $tag->color ?? 'Gray';
+   $hsl = hex2hsl($color);
+   $textColor = 'text-black';
+   
+   /* Convert hex color to hsl */
+   // Get lightness value from hsl
+   $lightness = $hsl[2];
+   // Set text color based on lightness value
+   if ($lightness > 0.4) {
+      $textColor = 'text-gray-900 font-bold';
+   } else {
+      $textColor = 'text-gray-50 font-semibold';
    }
 @endphp
 
