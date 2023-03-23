ca932dea59d1ba7a061807602686f8c3f3693405
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-23 02:50:38 +0100

    Convert to form to prevent weird flexbox glitch

--- resources/views/profile/info/amt-followers.blade.php
+++ resources/views/profile/info/amt-followers.blade.php
@@ -1,6 +1,13 @@
-<x-button flat purple md title="{{ $user->followers->count() }} {{ Str::plural('follower', $user->followers->count()) }}">
-   <div class="flex flex-row items-center gap-2 font-semibold">
-      <i class="fas fa-people-carry"></i>
-      {{ $user->followers->count() }} @if (isset($extend) && $extend){{ Str::plural('follower', $user->followers->count()) }}@endif
-   </div>
-</x-button>
+<form method="GET" action="{{ route('users.friends', ['user' => $user])}}">
+   <x-button 
+      type="submit"
+      flat purple md 
+      title="{{ $user->followers->count() }} {{ Str::plural('follower', $user->followers->count()) }}"
+   >
+      <div class="flex flex-row items-center gap-2 font-semibold">
+         <i class="fas fa-people-carry"></i>
+         {{ $user->followers->count() }} 
+         @if(isset($extend) && $extend) {{ Str::plural('follower', $user->followers->count()) }} @endif
+      </div>
+   </x-button>
+</form>
--- resources/views/profile/info/amt-following.blade.php
+++ resources/views/profile/info/amt-following.blade.php
@@ -1,6 +1,13 @@
-<x-button flat indigo md title="Following {{ $user->following->count() }} {{ Str::plural('user', $user->following->count()) }}">
-   <div class="flex flex-row items-center gap-2 font-semibold">
-      <i class="fas fa-users"></i>
-      @if (isset($extend) && $extend)Following @endif{{ $user->following->count() }} @if (isset($extend) && $extend){{ Str::plural('user', $user->following->count()) }}@endif
-   </div>
-</x-button>
+<form method="GET" action="{{ route('users.friends', ['user' => $user])}}">
+   <x-button 
+      flat indigo md 
+      title="Following {{ $user->following->count() }} {{ Str::plural('user', $user->following->count()) }}"
+   >
+      <div class="flex flex-row items-center gap-2 font-semibold">
+         <i class="fas fa-users"></i>
+         @if(isset($extend) && $extend) Following @endif
+         {{ $user->following->count() }} 
+         @if(isset($extend) && $extend) {{ Str::plural('user', $user->following->count()) }}@endif
+      </div>
+   </x-button>
+</form>
